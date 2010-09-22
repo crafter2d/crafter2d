b8482d457da5247210832086df20cb0745df2be5
Jeroen Broekhuizen <jengine.sse@live.nl>
2010-09-22 12:57:26 +0000

    * added silent argument to tolua batch file

--- tolua/run.bat
+++ tolua/run.bat
@@ -1,14 +1,19 @@
 @echo off
-echo Running tolua to generate code...
-echo Jeroen Broekhuizen, 2005
 
-tolua++ -o tolua_general.cpp -H tolua_general.h -n general general.cpp
-tolua++ -o tolua_gui.cpp -H tolua_gui.h -n gui gui.cpp
-tolua++ -o tolua_network.cpp -H tolua_network.h -n network network.cpp
-tolua++ -o tolua_editor.cpp -H tolua_editor.h -n editor editor.cpp
-tolua++ -o tolua_physics.cpp -H tolua_physics.h -n physics physics.cpp
+set silent=true
+if not "%1" == "--silent" set silent=
 
-echo Copying to the script directory...
-echo copy /Y tolua_* ..\src\tolua_*
+if not defined silent (
+	echo Running tolua to generate code...
+	echo Jeroen Broekhuizen, 2005
+)
 
-echo Ready!
+tolua++ -o ..\src\engine\tolua_general.cpp -H ..\src\engine\tolua_general.h -n general general.cpp
+tolua++ -o ..\src\engine\tolua_gui.cpp -H ..\src\engine\tolua_gui.h -n gui gui.cpp
+tolua++ -o ..\src\engine\tolua_network.cpp -H ..\src\engine\tolua_network.h -n network network.cpp
+tolua++ -o ..\src\engine\tolua_editor.cpp -H ..\src\engine\tolua_editor.h -n editor editor.cpp
+tolua++ -o ..\src\engine\tolua_physics.cpp -H ..\src\engine\tolua_physics.h -n physics physics.cpp
+
+if not defined silent (
+	echo Ready!
+)
