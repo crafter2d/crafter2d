a0a81ea236b37dfbab1be73b016462328cfea423
Jeroen Broekhuizen <jengine.sse@live.nl>
2010-11-02 06:04:22 +0000

    * added missing file

---
+++ src/engine/keys.h
@@ -0,0 +1,132 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Jeroen Broekhuizen                              *
+ *   jengine.sse@live.nl                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef KEYS_H_
+#define KEYS_H_
+
+namespace Keys
+{
+   enum {
+      eKey_tab,
+      eKey_space,
+      eKey_backspace,
+      eKey_lshift,
+      eKey_rshift,
+      eKey_lalt,
+      eKey_ralt,
+      eKey_lcontrol,
+      eKey_rcontrol,
+      eKey_escape,
+      eKey_break,
+      eKey_backquote,      // `
+      eKey_a,
+      eKey_b,
+      eKey_c,
+      eKey_d,
+      eKey_e,
+      eKey_f,
+      eKey_g,
+      eKey_h,
+      eKey_i,
+      eKey_j,
+      eKey_k,
+      eKey_l,
+      eKey_m,
+      eKey_n,
+      eKey_o,
+      eKey_p,
+      eKey_q,
+      eKey_r,
+      eKey_s,
+      eKey_t,
+      eKey_u,
+      eKey_v,
+      eKey_w,
+      eKey_x,
+      eKey_y,
+      eKey_z,
+      eKey_1,
+      eKey_2,
+      eKey_3,
+      eKey_4,
+      eKey_5,
+      eKey_6,
+      eKey_7,
+      eKey_8,
+      eKey_9,
+      eKey_colon,
+      eKey_semicolon,
+      eKey_minus,
+      eKey_plus,
+      eKey_less,
+      eKey_greater,
+      eKey_equal,
+      eKey_leftbracket,    // [
+      eKey_rightbracket,   // ]
+      eKey_backslash,      // /
+      eKey_caret,          // |
+      eKey_underscore,     // _
+      eKey_enter,
+      eKey_home,
+      eKey_pageUp,
+      eKey_pageDown,
+      eKey_end,
+      eKey_insert,
+      eKey_delete,
+      eKey_up,
+      eKey_down,
+      eKey_left,
+      eKey_right,
+      eKey_numlock,
+      eKey_capslock,
+      eKey_scrollock,
+      eKey_F1,
+      eKey_F2,
+      eKey_F3,
+      eKey_F4,
+      eKey_F5,
+      eKey_F6,
+      eKey_F7,
+      eKey_F8,
+      eKey_F9,
+      eKey_F10,
+      eKey_F11,
+      eKey_F12,
+      eKey_kp_dot,
+      eKey_kp_min,
+      eKey_kp_plus,
+      eKey_kp_multiply,
+      eKey_kp_divide,
+      eKey_kp_equal,
+      eKey_kp_insert,
+      eKey_kp_delete,
+      eKey_kp_enter,
+      eKey_kp_1,
+      eKey_kp_2,
+      eKey_kp_3,
+      eKey_kp_4,
+      eKey_kp_5,
+      eKey_kp_6,
+      eKey_kp_7,
+      eKey_kp_8,
+      eKey_kp_9,
+   };
+};
+
+#endif // KEYS_H_
