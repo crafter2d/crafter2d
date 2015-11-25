e5a84e84bd7605ae4197ffbe721eda2e40b3089e
Jeroen Broekhuizen <jbroekhuizen@hotmail.nl>
2015-11-25 09:04:51 +0100

    Travis continuous build preparations

---
+++ projects/install-premake5.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -ex
+wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-linux.tar.gz -O /tmp/premake5.tar.gz
+tar -xvf /tmp/premake5.tar.gz
+export PATH=$PATH:$PWD/premake5
