8db54cc5a6a5091023624799cf42821f27ea4933
crafter2d <crafter2d@outlook.com>
2015-11-27 20:56:55 +0100

    Fixing travis scripts

--- projects/install-premake5.sh
+++ projects/install-premake5.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-linux.tar.gz -O /tmp/premake5.tar.gz
 tar -xvf /tmp/premake5.tar.gz
-export PATH=$PATH:$PWD/premake5
+export PATH=$PATH:$PWD/premake5/bin/release
