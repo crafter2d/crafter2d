f8215891df3d5f441df3977404ca9537ffdb1540
crafter2d <crafter2d@outlook.com>
2015-11-29 09:08:55 +0100

    Compile premake5 instead of using precompiled binary which does not work on Travis

--- projects/install-premake5.sh
+++ projects/install-premake5.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -ex
-wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-linux.tar.gz -O /tmp/premake5.tar.gz
-tar -xvf /tmp/premake5.tar.gz
-export PATH=$PATH:$PWD/premake5/bin/release
+wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-src.zip -O /tmp/premake5-src.zip
+unzip /tmp/premake5-src.zip
+cd premake-5.0.0-alpha6/build/gmake.unix
+make
+cd ../../../
+export PATH=$PATH:$PWD/premake-5.0.0-alpha6/bin/release
