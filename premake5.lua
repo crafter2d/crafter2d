15a6eb8f300274d72e8a44d96b78eaf849dd9752
Jeroen Broekhuizen <jbroekhuizen@hotmail.nl>
2015-12-09 08:03:28 +0100

    Update for travis building

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,12 @@ newoption
 	description = "Path to the external libraries"
 }
 
+newoption
+{
+	trigger = "travis"
+	description   = "Generates make files for Travis-testing"
+}
+
 if not _OPTIONS["libdir"] then
 	_OPTIONS["libdir"] = path.join(path.join(os.getcwd(), "../externallibs"), _ACTION)
 end
@@ -30,15 +36,18 @@ solution "Crafter2D"
 package.path = package.path .. ";./projects/?.lua"
  
 require "proj-core"
-require "proj-engine"
-require "proj-game"
-require "proj-gen"
-require "proj-unittest"
-require "proj-compiler"
-require "proj-modyas"
-require "proj-modc2d"
-require "proj-modfmod"
-require "proj-modd3d"
-require "proj-modogl"
-require "proj-modui"
-require "proj-yasc"
+
+if not _OPTIONS["travis"] then
+	require "proj-engine"
+	require "proj-game"
+	require "proj-gen"
+	require "proj-unittest"
+	require "proj-compiler"
+	require "proj-modyas"
+	require "proj-modc2d"
+	require "proj-modfmod"
+	require "proj-modd3d"
+	require "proj-modogl"
+	require "proj-modui"
+	require "proj-yasc"
+end
