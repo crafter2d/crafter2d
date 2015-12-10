a951d97b825ee4948f888367f5ff3a676ce0c358
crafter2d <crafter2d@outlook.com>
2015-12-10 12:32:57 +0100

    Fixed build path in Box2D script

--- projects/install-box2d.sh
+++ projects/install-box2d.sh
@@ -2,7 +2,7 @@
 set -ex
 wget https://github.com/erincatto/Box2D/archive/master.zip -O /tmp/box2d-master.zip
 unzip /tmp/box2d-master.zip
-cd Box2D-master/build
+cd Box2D-master/Box2D/Build
 cmake -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_EXAMPLES=OFF ..
 make
-cd ../..
+cd ../../..
