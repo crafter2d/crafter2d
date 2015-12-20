4171287be62587fe5df990816ba6c4f135bde09c
crafter2d <crafter2d@outlook.com>
2015-12-20 13:14:13 +0100

    Install with sudo

--- projects/install-box2d.sh
+++ projects/install-box2d.sh
@@ -4,6 +4,5 @@ wget https://github.com/erincatto/Box2D/archive/master.zip -O /tmp/box2d-master.
 unzip /tmp/box2d-master.zip
 cd Box2D-master/Box2D/Build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_EXAMPLES=OFF ..
-make
-make install
+make && sudo make install
 cd ../../..
