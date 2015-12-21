39a9918ef30b3429afaaf66ac4b49a240920358d
crafter2d <crafter2d@outlook.com>
2015-12-21 08:15:19 +0100

    Fixes for broken premake5 package

--- projects/install-premake5.sh
+++ projects/install-premake5.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -ex
 #wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-src.zip -O premake5-src.zip
-wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha7/premake-5.0.0-alpha7-linux.tar.gz -O premake5.tar.gz
-tar -zxvf premake5.tar.gz
 #unzip premake5-src.zip
 #cd premake-5.0.0-alpha6/build/gmake.unix
 #make
 #cd ../../../
+
+git clone https://github.com/premake/premake-core.git premake-core
+cd premake-core
+make -f Bootstrap.mak linux
