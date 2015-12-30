4b2575c975109210adc5453ae2d4c33932fe51e7
crafter2d <crafter2d@outlook.com>
2015-12-30 16:52:47 +0100

    Updated mod_ogl lua project file.

--- projects/proj-modogl.lua
+++ projects/proj-modogl.lua
@@ -31,14 +31,12 @@ project "mod_ogl"
 		includedirs {
 			path.join(libdir, "glee/include"),
 			path.join(libdir, "sdl/include"),
-			path.join(libdir, "soil/include"),
 			path.join(libdir, "freetype2/include"),
 		}
 
 		libdirs {
 			path.join(libdir, "glee/lib"),
 			path.join(libdir, "sdl/lib"),
-			path.join(libdir, "soil/lib"),
 			path.join(libdir, "freetype2/lib"),
 		}
 		
@@ -46,28 +44,15 @@ project "mod_ogl"
 		buildoptions { "-std=c++0x", "-W", "-Wall", "-O0" }
 		defines { "LINUX" }
 		includedirs { "/usr/include", "/usr/include/freetype2", "/usr/local/include" }
-		links { "GLee", "freetype", "SOIL" }
+		links { "GLEW", "freetype" }
 
 	-- set IDE specific settings
 	filter "action:vs*"
 		links { "opengl32", "glu32" }
 		
 	filter { "action:vs*", "Debug" }
-		links { "GLee_d", "SOIL_d", "freetype242MT_D" }
+		links { "Glew_d", "freetype242MT_D" }
 				
 	filter { "action:vs*", "Release" }
-		links { "GLee", "SOIL", "freetype242MT_D" }
-		
-	filter "action:cb-gcc"
-		buildoptions { "-W", "-Wall", "-O0" }
-		linkoptions { "--allow-multiple-definition", "-Xlinker", "-zmuldefs" }
-       
-	filter { "action:cb-gcc", "Debug" }
-		links { "GLee_d", "SOIL_d", "freetype242MT_D" } 
-	 
-	filter { "action:cb-gcc", "Release" }
-		links { "GLee_d", "SOIL", "freetype242MT" }
+		links { "Glew", "freetype242MT" }
 
-	
-   
-	
