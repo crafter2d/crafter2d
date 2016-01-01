41d05be008c5d6230a2097598ece4256ac1b1a5d
crafter2d <crafter2d@outlook.com>
2016-01-01 11:39:43 +0100

    Add libantrl3 library to travis build

---
+++ projects/install-libantlr3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -ex
+wget http://www.antlr3.org/download/C/libantlr3c-3.4.tar.gz -O libantlr3.tar.gz
+tar -zxvf libantlr3.tar.gz
+cd libantlr3c-3.4 && ./configure --prefix=/usr && make && sudo make install
+cd ..
+
+
