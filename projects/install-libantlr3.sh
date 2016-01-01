61ec1b9bcf2542e24e2d03aed5e7184cf0b258fc
crafter2d <crafter2d@outlook.com>
2016-01-01 11:52:51 +0100

    Build antlr as 64 bit library

--- projects/install-libantlr3.sh
+++ projects/install-libantlr3.sh
@@ -2,7 +2,7 @@
 set -ex
 wget http://www.antlr3.org/download/C/libantlr3c-3.4.tar.gz -O libantlr3.tar.gz
 tar -zxvf libantlr3.tar.gz
-cd libantlr3c-3.4 && ./configure --prefix=/usr && make && sudo make install
+cd libantlr3c-3.4 && ./configure --prefix=/usr --enable-64bit && make && sudo make install
 cd ..
 
 
