25d1470027234f91d7634d5bcd84f7a40050f0d6
crafter2d <jbroekhuizen@hotmail.nl>
2016-05-16 14:00:13 +0200

    Fixed Box2D build, zip file contained other folder

--- projects/install-box2d.sh
+++ projects/install-box2d.sh
@@ -2,7 +2,7 @@
 set -ex
 wget https://github.com/erincatto/Box2D/archive/v2.3.1.zip -O /tmp/box2d-master.zip
 unzip /tmp/box2d-master.zip
-cd Box2D-master/Box2D/Build
+cd Box2D-2.3.1/Box2D/Build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_EXAMPLES=OFF ..
 make && sudo make install
 cd ../../..
