8ef5c496a1c899025f6a7183e3a604ecd1f718f9
crafter2d <jbroekhuizen@hotmail.nl>
2016-05-16 13:53:35 +0200

    Use tagged version of Box2D instead of master (unstable)

--- projects/install-box2d.sh
+++ projects/install-box2d.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wget https://github.com/erincatto/Box2D/archive/master.zip -O /tmp/box2d-master.zip
+wget https://github.com/erincatto/Box2D/archive/v2.3.1.zip -O /tmp/box2d-master.zip
 unzip /tmp/box2d-master.zip
 cd Box2D-master/Box2D/Build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_EXAMPLES=OFF ..
